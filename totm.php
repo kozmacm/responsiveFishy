2eac9587792d761c8cd1c85c89b4f88cf5e6099c
kozmacm <kozmacm@email.sc.edu>
2015-11-20 09:31:09 -0500

    Fix invalid file alert error.

--- totm.php
+++ totm.php
@@ -113,8 +113,7 @@
             echo '<div class="alert alert-danger fade in">
                   <a href="#" class="close" data-dismiss="alert">&times;</a>
                   <strong>Error: </strong> - Invalid File.
-            
-            [      </div>';
+                  </div>';
             
             //echo "Error - Invalid file";
         }
