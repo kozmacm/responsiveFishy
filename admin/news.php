0aa98d62e5d1e89218e5cca80905c91ed618c84d
samsitconsulting <mcclain.sam@gmail.com>
2016-01-05 18:39:16 -0500

    Modified the username variable in the Header.

--- admin/news.php
+++ admin/news.php
@@ -298,7 +298,7 @@
                     <ul class="nav navbar-nav navbar-right">
                         <li class="dropdown">
                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    Admin
+                                   <?php echo $_SESSION['username']; ?>
                                     <b class="caret"></b>
                               </a>
                               <ul class="dropdown-menu">
