e62dcb578c41ec873d8e276c7e16241c1c8fd052
kozmacm <kozmacm@email.sc.edu>
2016-01-10 15:03:58 -0500

    Fix dashboard date range.

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -16,7 +16,7 @@
 
     $ga = new gapi("fishy-business-service@fishy-business-1182.iam.gserviceaccount.com", "keys/Fishy Business-a9f7453a79f4.p12");
     //$filter = 'country == United States && browser == Firefox || browser == Chrome';
-    $ga->requestReportData(91085606,array('browser','browserVersion'),array('pageviews','visits'),'-visits',$filter, NULL, NULL, 1, 10);
+    $ga->requestReportData(91085606,array('browser','browserVersion'),array('pageviews','visits'),'-visits',$filter, '30daysAgo', NULL, 1, 10);
 ?>
 
 <!doctype html>
