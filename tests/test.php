a2795b6d0cad32eb1849613b4c9a7873f28524dd
kozmacm <kozmacm@email.sc.edu>
2016-01-21 14:57:58 -0500

    Upload some test index pages so that we can show Guy and hopefully purchase some quality images.

---
+++ tests/test.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Test page to show background images</title>
+    </head>
+    <body>
+        <h2>Click to test out some new backgrounds!</h2>
+        <a href="test1.php">Test 1</a></br>
+        <a href="test2.php">Test 2</a></br>
+        <a href="test3.php">Test 3</a></br>
+        <a href="test4.php">Test 4 *</a></br>
+        <a href="test5.php">Test 5 *</a></br>
+        <a href="test6.php">Test 6 *</a></br>
+        <a href="test7.php">Test 7</a></br>
+        <a href="test8.php">Test 8 *</a></br>
+        <a href="test9.php">Test 9</a></br>
+    </body>
+</html>
