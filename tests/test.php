c69ae14a76988a33e9261d8cfe8ddc2dbbea680c
kozmacm <kozmacm@email.sc.edu>
2016-01-25 11:56:04 -0500

    Delete test images that were not aquarium images. Testing some new images, however am not liking any of them very much currently. Just in testing.

--- tests/test.php
+++ tests/test.php
@@ -13,11 +13,5 @@
         <a href="test1.php">Test 1</a></br>
         <a href="test2.php">Test 2</a></br>
         <a href="test3.php">Test 3</a></br>
-        <a href="test4.php">Test 4 *</a></br>
-        <a href="test5.php">Test 5 *</a></br>
-        <a href="test6.php">Test 6 *</a></br>
-        <a href="test7.php">Test 7</a></br>
-        <a href="test8.php">Test 8 *</a></br>
-        <a href="test9.php">Test 9</a></br>
     </body>
 </html>
