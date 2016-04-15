2f0ae8769d228bdffe734372a5d62a1408be3bd7
kozmacm <kozmacm@email.sc.edu>
2016-04-15 11:08:12 -0400

    Backend totm didnt commit, this is a test.

--- admin/totm.php
+++ admin/totm.php
@@ -226,6 +226,7 @@
                                         }
                                     } 
                                 $stmt->free();
+
                                 //$mysqli->close();    
                                 ?>       
                             </div>
