2402c4dd08c5a0452339c021c134324fbee36a72
kozmacm <kozmacm@email.sc.edu>
2016-10-20 14:41:30 -0400

    Minor edits

--- index.php
+++ index.php
@@ -164,8 +164,6 @@
     <div class="parallax filter-black">
         <div class="parallax-image">
             <img src="assets/img/JacksElectricCornfield.jpg">
-            <!--<img src="assets/img/backgroundthumb.jpg-->
-
         </div>    
         <div class="small-info">
             <h1>Fishy Business</h1>
